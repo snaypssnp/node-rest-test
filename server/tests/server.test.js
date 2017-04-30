016fc3c40ce1f85490dd7ac7612c0a431186fa34
Sergey Popov <snaypssnp@gmail.com>
2017-04-30 07:30:41 -0400

    Test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -108,3 +108,43 @@ describe('GET /todos/:id', () => {
       .end(done);
   });
 });
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    var hexId = todos[1]._id.toHexString();
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.findById(hexId)
+          .then((result) => {
+            expect(result).toNotExist();
+            done();
+          })
+          .catch((err) => done(err));
+      })
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    const hexId = new ObjectID().toHexString();
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('hould return 404 if object id is invalid', (done) => {
+    request(app)
+      .delete('/todos/123abcq')
+      .expect(404)
+      .end(done);
+  });
+});
