6ecd3c9b7c846b84c8b8852cf8548812228a43f0
Sergey Popov <snaypssnp@gmail.com>
2017-04-30 06:15:00 -0400

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -2,10 +2,11 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
-const { mongoose } = require('./db/mongoose');
-const { Todo } = require('./models/todo');
-const { User } = require('./models/user');
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
 
 const app = express();
 
@@ -18,12 +19,8 @@ app.post('/todos', (req, res) => {
 
   todo
     .save()
-    .then((doc) => {
-      res.send(doc);
-    })
-    .catch((e) => {
-      res.status(400).send(e);
-    });
+    .then((doc) => res.send(doc))
+    .catch((e) => res.status(400).send(e));
 });
 
 
@@ -34,10 +31,27 @@ app.get('/todos', (req, res) => {
         todos,
       });
     })
-    .catch((err) => {
-      res.status(400).send(e);
-    })
+    .catch((err) => res.status(400).send(e));
 });
+
+
+app.get('/todos/:id', (req, res) => {
+  const {id}  = req.params;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((err) => {
+    res.status(400).send();
+  });
+});
+
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
