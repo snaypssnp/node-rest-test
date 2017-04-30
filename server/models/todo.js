bfb688328ada690d7dd080d4b436f8bd52d1f5b2
Sergey Popov <snaypssnp@gmail.com>
2017-04-30 05:15:47 -0400

    Add GET /todos route

--- server/models/todo.js
+++ server/models/todo.js
@@ -1,3 +1,4 @@
+'use strict';
 const mongoose = require('mongoose');
 
 const Todo = mongoose.model('Todo', {
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 'use strict';
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -23,6 +24,19 @@ app.post('/todos', (req, res) => {
     .catch((e) => {
       res.status(400).send(e);
     });
+});
+
+
+app.get('/todos', (req, res) => {
+  Todo.find()
+    .then((todos) => {
+      res.send({
+        todos,
+      });
+    })
+    .catch((err) => {
+      res.status(400).send(e);
+    })
 });
 
 app.listen(3000, () => {
