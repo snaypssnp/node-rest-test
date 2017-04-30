686c953fef251742113358a31d0c14805c937849
Sergey Popov <snaypssnp@gmail.com>
2017-04-30 05:05:53 -0400

    initial commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,47 @@
+'use sctrict';
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) =>{
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connect to MongoDB server');
+
+  // db.collection('Todos')
+  //   .find({completed: false})
+  //   .toArray()
+  //   .then((docs) => {
+  //     console.log('Todos');
+  //     console.log(JSON.stringify(docs, undefined, 2));
+  //   })
+  //   .catch((err) => {
+  //     console.log('Unable to fetch todos', err);
+  //   });
+
+  // db.collection('Todos')
+  //   .find()
+  //   .count()
+  //   .then((count) => {
+  //     console.log(`Todos count: ${count}`);
+  //   })
+  //   .catch((err) => {
+  //     console.log('Unable to fetch todos', err);
+  //   });
+
+  db.collection('Users')
+    .find({name: 'Serge'})
+    .toArray()
+    .then((docs) => {
+      console.log(JSON.stringify(docs, undefined, 2));
+    })
+    .catch((err) => {
+      console.log('Unable to fetch todos', err);
+    });
+
+
+  db.close();
+});
+
+
+
+
